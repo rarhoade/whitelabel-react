0b55a261984a6b0ffc3b1bb2a90f76d206bf9f5f
Rahul Rhoades <rarhoade@umich.edu>
2019-11-14 22:40:04 -0500

    finished contact form functionality

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ class App extends React.Component{
       homePage: true,
       tutorialPage: false,
       blogPage: false,
-      contactPage: false
+      contactPage: false,
+      name: null,
+      email: null,
+      message: null
     };
 
     this.enableContactPage = this.enableContactPage.bind(this);
-    this.enableHomePage = this.enableHomePage.bind(this)
+    this.enableHomePage = this.enableHomePage.bind(this);
+    this.handleContactFormChange = this.handleContactFormChange.bind(this);
+    this.handleContactFormSubmit = this.handleContactFormSubmit.bind(this);
   }
 
   enableContactPage() {
@@ -38,23 +43,49 @@ class App extends React.Component{
     })
   }
 
+  handleContactFormChange(event){
+    switch(event.target.getAttribute("formattr")){
+      case "name":
+        this.setState({name: event.target.value});
+        break;
+      case "email":
+        this.setState({email: event.target.value});
+        break;
+      case "message":
+        this.setState({message: event.target.value});
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  handleContactFormSubmit(event){
+    var submission = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message
+    };
+    alert(submission.name + 
+      " with email: " + submission.email 
+      + " with message: " + submission.message);
+  }
+
   contactPage(){
     return(
-      <form>
-        <div className="App-header">
-          <img src={logo} className="Contact-logo" alt="logo"/>
-          <form>
-            <h2 className="Input-form">FULL NAME</h2>
-            <input type="text" placeholder = "Enter your full name"/>
-            <h2 className="Input-form">EMAIL</h2>
-            <input type="text" placeholder = "Enter your email"/>
-            <h2 className="Input-form">MESSAGE</h2>
-            <textarea placeholder = "What are your plans?"/>
-            <br/>
-            <input type="submit" value="Submit Message ->"/>
-          </form>
-        </div>
-      </form>
+      <div className="App-header">
+        <img src={logo} className="Contact-logo" alt="logo"/>
+        <form onSubmit={this.handleContactFormSubmit}>
+          <h2 className="Input-form">FULL NAME</h2>
+          <input formattr="name" onChange={this.handleContactFormChange} type="text" placeholder = "Enter your full name"/>
+          <h2 className="Input-form">EMAIL</h2>
+          <input formattr="email" onChange={this.handleContactFormChange} type="text" placeholder = "Enter your email"/>
+          <h2 className="Input-form">MESSAGE</h2>
+          <textarea formattr="message" onChange={this.handleContactFormChange} placeholder = "What are your plans?"/>
+          <br/>
+          <input type="submit" value="Submit Message ->"/>
+        </form>
+      </div>
     );
   }
 
